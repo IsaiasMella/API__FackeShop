3eb81602466ca0391a1124e7b84adfe3a97f3a44
isaiasmella <isaias.mella2013@gmail.com>
2022-12-05 01:43:21 -0300

    end points

--- app.js
+++ app.js
@@ -13,18 +13,69 @@ app.use(cors())
 //servicios
 app.post('/register', async (req, res) => {
     const { name, last_name, password, email } = req.body
-    const [rows] = await pool.query(
-        'INSERT INTO users (name,last_name,password,email) VALUES(?,?,?,?)',
-        [name, last_name, password, email]
+    const [emailComprobation] = await pool.query(
+        'SELECT email FROM users where email = ?', [email]
     )
-    res.status(200).json({ rows })
+
+    if (!emailComprobation[0]) {
+        const [rows] = await pool.query(
+            'INSERT INTO users (name,last_name,password,email) VALUES(?,?,?,?)',
+            [name, last_name, password, email]
+        )
+        res.status(200).json({ rows })
+    } else {
+        res.status(404).send('El usuario ya tiene una cuenta')
+    }
+
 })
 
-app.get('/', async (req, res) => {
-    const [rows] = await pool.query(
-        'SELECT * FROM users where id=1'
+app.post('/login', async (req, res) => {
+    const { password, email } = req.body
+
+    const [emailComprobation] = await pool.query(
+        'SELECT * FROM users where email = ? and password = ?', [email, password]
     )
-    res.status(200).json(rows[0])
+
+    if (emailComprobation[0]) {
+        res.status(200).json(emailComprobation[0])
+    } else {
+        res.status(404).send('El usuario no posee una cuenta')
+    }
+
+})
+
+
+app.put('/user', async (req, res) => {
+    const { name, last_name, password, email } = req.body
+    const [emailComprobation] = await pool.query(
+        'SELECT id FROM users where email = ?', [email]
+    )
+
+    if (emailComprobation[0]) {
+        let id = emailComprobation[0].id
+        const [rows] = await pool.query(
+            'UPDATE users SET name=?, last_name=?, password=?, email=? WHERE id = ?',
+            [name, last_name, password, email, id]
+        )
+        res.status(200).json({ rows })
+    } else {
+        res.status(404).send('El usuario ya tiene una cuenta')
+    }
+})
+
+app.delete('/user/delete', async (req, res) => {
+    const { id } = req.body
+    console.log(req.body)
+// console.log(emailComprobation[0].id)
+    if (id) {
+        const [rows] = await pool.query(
+            'DELETE FROM users where id = ?',
+            [id]
+        )
+        res.status(200).json({ rows })
+    } else {
+        res.status(404).send('El usuario ya tiene una cuenta')
+    }
 })
 
 app.listen(3001, () => {
